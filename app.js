718a18d1f966384bd73dde1f8c3600bddf06b793
Ryo Suzuki <ryosuzk@gmail.com>
2016-02-10 16:31:23 -0700

    Experimental facial detection

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var http = require('http');
 var route = require('koa-route');
 var views = require('co-views');
 var serve = require('koa-static');
+var favicon = require('koa-favicon');
 var parser = require('koa-bodyparser');
 var koa = require('koa');
 
@@ -13,6 +14,7 @@ var server = http.createServer(app.callback());
 var port = process.env.PORT || 3000;
 
 app.use(serve('.'));
+app.use(favicon('/assets/favicon.ico'));
 app.use(parser({
   strict: false,
   jsonLimit: '500mb'
@@ -24,10 +26,15 @@ app.use( function *(next) {
   yield next;
 });
 app.use(route.get('/', index));
+app.use(route.get('/:id', show));
 
-function *index() {
+function *index () {
   this.body = yield this.render('index');
 }
+function *show (id) {
+  this.body = yield this.render(id);
+}
+
 
 app.listen(port);
 
