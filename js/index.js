ad4cf1d1cd4bf9c90c659e12de93030b1f2e5ff1
Ryo Suzuki <ryosuzk@gmail.com>
2016-02-09 22:55:47 -0700

    First prototype to connect start or end point

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 
 paper.install(window);
 
-var path;
 var start;
 var end;
-var line;
+var lines = [];
+var points = [];
 
 var pathStyle = {
   strokeColor: 'black',
@@ -15,7 +15,8 @@ var pathStyle = {
 window.onload = function () {
   paper.setup('canvas');
 
-  tool = new Tool();
+  var path;
+  var tool = new Tool();
   tool.onMouseDown = function (event) {
     if (path) {
       path.selected = false;
@@ -39,12 +40,66 @@ window.onload = function () {
     end = event.point;
     path.remove();
 
+    points = [];
+    for (var i=0; i<lines.length; i++) {
+      var line = lines[i];
+      points.push(distance(start, line.start));
+      points.push(distance(start, line.end));
+      points.push(distance(end, line.start));
+      points.push(distance(end, line.end));
+    }
+
+    console.log(points);
     var line = new Path(pathStyle);
-    line.add(new Point(start.x, start.y));
-    line.add(new Point(end.x, end.y));
+    if (!points.length) {
+      line.add(new Point(start.x, start.y));
+      line.add(new Point(end.x, end.y));
+    } else {
+      var min = _.min(points);
+      var index = points.indexOf(min);
+      // 0 -> start - lines[0].start
+      // 1 -> start - lines[0].end
+      // 2 -> end   - lines[0].start
+      // 3 -> end   - lines[0].end
+      // 4 -> start - lines[1].start
+      // 5 -> start - lines[1].end
+      // 6 -> end   - lines[1].start
+      // 7 -> end   - lines[1].end
+      // ...
+      var i = Math.floor(index/4);
+      var p = (index%2 == 0) ? 'start' : 'end';
+      if (index%4 < 2) {
+        line.add(lines[i][p]);
+        line.add(end);
+      } else {
+        line.add(start);
+        line.add(lines[i][p]);
+      }
+
+
+    }
+    lines.push({
+      start: _.first(line.segments).point,
+      end:   _.last(line.segments).point,
+    });
 
   }
 
 }
+
+function distance (p1, p2) {
+  return (p1.x - p2.x)*(p1.x - p2.x) + (p1.y - p2.y)*(p1.y - p2.y)
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
